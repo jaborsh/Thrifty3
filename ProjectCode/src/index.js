b581d086e5dfcb6b4778c6ed5bd0aebbe0b03b74
emni7932 <emni7932@colorado.edu>
2023-04-10 17:17:10 -0600

    Updated index file with base code - need to add/write apis

---
+++ ProjectCode/src/index.js
@@ -0,0 +1,26 @@
+// importing the dependencies
+// Express is a NodeJS framework that, among other features, allows us to create HTML templates.
+const express = require('express');
+const bodyParser = require('body-parser');
+const pgp = require('pg-promise')();
+require('dotenv').config();
+
+// defining the Express app
+const app = express();
+// using bodyParser to parse JSON in the request body into JS objects
+app.use(bodyParser.json());
+// Database connection details
+const dbConfig = {
+  host: 'db',
+  port: 5432,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+};
+// Connect to database using the above details
+const db = pgp(dbConfig);
+
+// starting the server and keeping the connection open to listen for more requests
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+  });
