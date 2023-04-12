aa4bbe1bd7f3ac62c7fec704398fdb6066b37eec
Devin Rocha <dero7036@colorado.edu>
2023-04-11 18:54:49 -0600

    Database init data added. Drafted header and added dependencies to existing pages. Minor fixes to .yaml

---
+++ ProjectCode/src/init_data/create.sql
@@ -0,0 +1,92 @@
+CREATE TABLE IF NOT EXISTS item_category (
+  category_id INT NOT NULL SERIAL,
+  name VARCHAR(255) NOT NULL,
+  base_price DECIMAL(2),
+  PRIMARY KEY (category_id)
+);
+
+-- -----------------------------------------------------
+-- Table preferences
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS preferences (
+  preference_ID INT NOT NULL SERIAL,
+  major VARCHAR(45) NOT NULL,
+  gender CHAR(1) NOT NULL,
+  category_ID VARCHAR(45) NOT NULL,
+  size VARCHAR(5),
+  PRIMARY KEY (preference_ID),
+  FOREIGN KEY (category_ID) REFERENCES item_category (category_ID)
+);
+
+-- -----------------------------------------------------
+-- Table users
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS users (
+  user_ID INT NOT NULL SERIAL,
+  username VARCHAR(16) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(32) NOT NULL,
+  first_name VARCHAR(45),
+  last_name VARCHAR(45),
+  gender CHAR(1),
+  major VARCHAR(45),
+  size_preference VARCHAR(5),
+  card_no CHAR(16) NOT NULL,
+  member_since DATE NOT NULL DEFAULT CURRENT_DATE,
+  is_paid BINARY(1) NOT NULL CONSTRAINT is_paid CHECK (is_paid in ('Y','N')),
+  preference_ID INT,
+  PRIMARY KEY (user_ID),
+  FOREIGN KEY (preference_ID) REFERENCES preferences (preference_ID)
+);
+-- -----------------------------------------------------
+-- Table item_images
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS item_images (
+  images_ID INT NOT NULL SERIAL,
+  url VARCHAR(255) NOT NULL,
+  item_ID INT NOT NULL,
+  PRIMARY KEY (images_ID),
+  FOREIGN KEY (item_ID) REFERENCES items (item_ID) ON DELETE CASCADE
+);
+
+-- -----------------------------------------------------
+-- Table items
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS items (
+  item_ID INT NOT NULL SERIAL,
+  name VARCHAR(255) NOT NULL,
+  user_ID INT,
+  category_ID INT NOT NULL,
+  color VARCHAR(45),
+  size VARCHAR(5),
+  images_id INT NOT NULL,
+  PRIMARY KEY (item_ID),
+  FOREIGN KEY (user_ID) REFERENCES users (username),
+  FOREIGN KEY (category_ID) REFERENCES item_category (category_id),
+  FOREIGN KEY (images_id) REFERENCES item_images (images_ID)
+);
+-- -----------------------------------------------------
+-- Table pickup_location
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS pickup_location (
+  location_id INT NOT NULL SERIAL,
+  building_name VARCHAR(45) NOT NULL,
+  num_listings INT,
+  PRIMARY KEY (location_id)
+ );
+
+-- -----------------------------------------------------
+-- Table listings
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS listings (
+  listing_ID INT NOT NULL SERIAL,
+  item_ID INT NOT NULL,
+  price DECIMAL(2) NOT NULL,
+  is_sale CHAR(1) NOT NULL DEFAULT 'N' CONSTRAINT is_sale CHECK (is_sale in ('Y','N')),
+  date_listed DATE NOT NULL DEFAULT CURRENT_DATE,
+  location_ID INT NOT NULL,
+  description VARCHAR(255),
+  PRIMARY KEY (listing_ID),
+  FOREIGN KEY (item_ID) REFERENCES items (item_ID) ON DELETE CASCADE,
+  FOREIGN KEY (location_ID) REFERENCES pickup_location (location_id) ON DELETE CASCADE
+);
