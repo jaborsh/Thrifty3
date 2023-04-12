33647d205137ea21575a47878a5d463aebb42a28
Devin Rocha <dero7036@colorado.edu>
2023-04-12 13:49:08 -0600

    DATABASE AHHHHHH WORKING

--- ProjectCode/src/init_data/create.sql
+++ ProjectCode/src/init_data/create.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS item_category (
-  category_id SERIAL NOT NULL,
+  category_ID SERIAL,
   name VARCHAR(255) NOT NULL,
   base_price DECIMAL(2),
-  PRIMARY KEY (category_id)
+  PRIMARY KEY (category_ID)
 );
 
 -- -----------------------------------------------------
 -- Table preferences
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS preferences (
-  preference_ID SERIAL NOT NULL,
+  preference_ID SERIAL,
   major VARCHAR(45) NOT NULL,
   gender CHAR(1) NOT NULL,
   category_ID INT NOT NULL,
@@ -23,9 +23,9 @@ CREATE TABLE IF NOT EXISTS preferences (
 -- Table users
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS users (
-  user_ID SERIAL NOT NULL,
+  user_ID SERIAL,
   username VARCHAR(16) NOT NULL,
-  email VARCHAR(255) NOT NULL UNIQUE,
+  email VARCHAR(255) NOT NULL,
   password VARCHAR(32) NOT NULL,
   first_name VARCHAR(45),
   last_name VARCHAR(45),
@@ -40,10 +40,26 @@ CREATE TABLE IF NOT EXISTS users (
   FOREIGN KEY (preference_ID) REFERENCES preferences (preference_ID)
 );
 -- -----------------------------------------------------
+-- Table items
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS items (
+  item_ID SERIAL,
+  name VARCHAR(255) NOT NULL,
+  user_ID INT,
+  category_ID INT NOT NULL,
+  color VARCHAR(45),
+  size VARCHAR(5),
+  --images_ID INT NOT NULL,
+  PRIMARY KEY (item_ID),
+  FOREIGN KEY (user_ID) REFERENCES users (user_ID),
+  FOREIGN KEY (category_ID) REFERENCES item_category (category_ID)
+  --FOREIGN KEY (images_ID) REFERENCES item_images (images_ID)
+);
+-- -----------------------------------------------------
 -- Table item_images
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS item_images (
-  images_ID SERIAL NOT NULL,
+  images_ID SERIAL,
   url VARCHAR(255) NOT NULL,
   item_ID INT NOT NULL,
   PRIMARY KEY (images_ID),
@@ -51,36 +67,20 @@ CREATE TABLE IF NOT EXISTS item_images (
 );
 
 -- -----------------------------------------------------
--- Table items
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS items (
-  item_ID SERIAL NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  user_ID INT,
-  category_ID INT NOT NULL,
-  color VARCHAR(45),
-  size VARCHAR(5),
-  images_id INT NOT NULL,
-  PRIMARY KEY (item_ID),
-  FOREIGN KEY (user_ID) REFERENCES users (username),
-  FOREIGN KEY (category_ID) REFERENCES item_category (category_id),
-  FOREIGN KEY (images_id) REFERENCES item_images (images_ID)
-);
--- -----------------------------------------------------
 -- Table pickup_location
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS pickup_location (
-  location_id INT NOT NULL,
+  location_ID SERIAL,
   building_name VARCHAR(45) NOT NULL,
   num_listings INT,
-  PRIMARY KEY (location_id)
+  PRIMARY KEY (location_ID)
  );
 
 -- -----------------------------------------------------
 -- Table listings
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS listings (
-  listing_ID SERIAL NOT NULL,
+  listing_ID SERIAL,
   item_ID INT NOT NULL,
   price DECIMAL(2) NOT NULL,
   is_sale CHAR(1) NOT NULL DEFAULT 'N' CONSTRAINT is_sale CHECK (is_sale in ('Y','N')),
